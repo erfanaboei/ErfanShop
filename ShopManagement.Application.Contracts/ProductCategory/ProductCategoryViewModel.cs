d7a0efffe546440346abae9d06e13c37b4783f62
erfan aboei <erfan.a3840@gmail.com>
2022-02-03 20:49:04 +0330

    ProductCategory Domain & ApplicationContracts Implemented

---
+++ ShopManagement.Application.Contracts/ProductCategory/ProductCategoryViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ShopManagement.Application.Contracts.ProductCategory
+{
+    public class ProductCategoryViewModel
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public string CreationDate { get; set; }
+        public long ProductsCount { get; set; }
+    }
+}
---
+++ ShopManagement.Domain/ProductCategoryAgg/ProductCategory.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _01_Framework.Domain;
+
+namespace ShopManagement.Domain.ProductCategoryAgg
+{
+    public class ProductCategory:EntityBase<long>
+    {
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public string Picture { get; private set; }
+        public string PictureTitle { get; private set; }
+        public string PictureAlt { get; private set; }
+        public string Keywords { get; private set; }
+        public string MetaDescription { get; private set; }
+        public string Slug { get; private set; }
+
+        protected ProductCategory()
+        {
+
+        }
+
+        public ProductCategory(string name, string description, string picture, string pictureTitle, string pictureAlt,
+            string keywords, string metaDescription, string slug)
+        {
+            Name = name;
+            Description = description;
+            Picture = picture;
+            PictureTitle = pictureTitle;
+            PictureAlt = pictureAlt;
+            Keywords = keywords;
+            MetaDescription = metaDescription;
+            Slug = slug;
+        }
+
+        public void Edit(string name, string description, string picture, string pictureTitle, string pictureAlt, string keywords, string metaDescription, string slug)
+        {
+            Name = name;
+            Description = description;
+            Picture = picture;
+            PictureTitle = pictureTitle;
+            PictureAlt = pictureAlt;
+            Keywords = keywords;
+            MetaDescription = metaDescription;
+            Slug = slug;
+        }
+    }
+}
