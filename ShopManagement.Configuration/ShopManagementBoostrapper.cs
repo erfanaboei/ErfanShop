a4afe0490df0b6a8b6465fcf9e15a28517f35fbc
erfan aboei <erfan.a3840@gmail.com>
2022-02-05 16:21:49 +0330

    پیاده سازی بخش ادمین قسمت دسته بندی محصولات

---
+++ ShopManagement.Configuration/ShopManagementBoostrapper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using ShopManagement.Application;
+using ShopManagement.Application.Contracts.ProductCategory;
+using ShopManagement.Domain.ProductCategoryAgg;
+using ShopManagement.Infrastructure.EfCore;
+using ShopManagement.Infrastructure.EfCore.Repository;
+
+namespace ShopManagement.Configuration
+{
+    public class ShopManagementBoostrapper
+    {
+        public static void Configure(IServiceCollection services , string connectionString)
+        {
+            services.AddTransient<IProductCategoryRepository, ProductCategoryRepository>();
+            services.AddTransient<IProductCategoryApplication, ProductCategoryApplication>();
+
+            services.AddDbContext<ShopContext>(x => x.UseSqlServer(connectionString));
+        }
+    }
+}
